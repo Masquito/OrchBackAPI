4db30a289677c727e939142fd23cd649964120f2
Aethillium <Aethillium@>
2023-12-16 20:58:16 +0100

    TEST - API works as intended and method from TestController displays data from database

---
+++ Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Orch_back_API.Entities;
+
+namespace Orch_back_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly MyJDBContext myJDBContext;
+
+        public TestController()
+        {
+            this.myJDBContext = new MyJDBContext();
+        }
+
+        [HttpGet]
+        public IActionResult get_data_test()
+        {
+            var user = myJDBContext.Users.First();
+            return Ok(user);
+        }
+    }
+}
